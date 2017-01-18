2098a260e7694de36a9dd755682335454a6b5899
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-01-19 00:48:17 +0400

    Updated index.js for ROUTING

--- src/index.js
+++ src/index.js
@@ -2,14 +2,28 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { configureStore } from './configureStore';
+import { syncHistoryWithStore } from 'react-router-redux';
+import { Router, Route, hashHistory, IndexRoute } from 'react-router';
 
 import App from './containers/App';
+import LoginPage from './containers/LoginPage';
+import IndexPage from './containers/IndexPage';
 
 const store = configureStore();
 
+// Create an enhanced history that syncs navigation events with the store
+const history = syncHistoryWithStore(hashHistory, store, {
+  adjustUrlOnReplay: false,
+});
+
 ReactDOM.render(
     <Provider store={store}>
-        <App />
+        <Router history={history}>
+            <Route path="/" component={App}>
+                <IndexRoute component={IndexPage} />
+                <Route path="/login" component={LoginPage} />
+            </Route>
+        </Router>
     </Provider>,
   document.getElementById('root')
 );
