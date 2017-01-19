c67eba2b02e7b934e92e8960e2e9cb84313372bc
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-01-19 23:35:03 +0300

    Updated components/User.js. Add example material-ui button

--- src/components/User.js
+++ src/components/User.js
@@ -1,12 +1,15 @@
 import React, { PropTypes, Component } from 'react';
 
+// material-ui imports
+import FlatButton from 'material-ui/FlatButton';
+
 export default class User extends Component {
   render() {
     const { name, updateName } = this.props;
     return <div>
       <p>Hello, {name}!</p>
       <div>
-        <button onClick={updateName}>Update name</button>
+        <FlatButton onClick={updateName} label="Update name" primary={true} />
       </div>
     </div>;
   }
