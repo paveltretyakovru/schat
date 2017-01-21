8ab74bac34355b9529f61f8ead8df2869a5961a5
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-01-21 20:58:30 +0400

    Created components/Header.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import AppBar from 'material-ui/AppBar';
+
+export default class Header extends Component {
+    render() {
+        return(<header>
+            <AppBar
+                title="Encrypted chat"
+                showMenuIconButton={ false }
+            />
+        </header>);
+    }
+}
