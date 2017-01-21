13cfb8a312e6d3964f8d779eceaf8166c132508a
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-01-21 21:05:53 +0400

    LeftSidebar was imported to App container

--- src/containers/App.js
+++ src/containers/App.js
@@ -4,6 +4,7 @@ import React, { Component } from 'react';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 
 import Header from '../components/Header';
+import LeftSidebar from '../components/LeftSidebar';
 
 import '../styles/app.css';
 
@@ -16,9 +17,16 @@ class App extends Component {
 
     return(<MuiThemeProvider>
       <div className="container">
+        
+        {/* Header component */}
         <Header />
 
-        { this.props.children }
+        {/* Main container */}
+        <main>
+          <LeftSidebar />
+          { this.props.children }
+        </main>
+
       </div>
     </MuiThemeProvider>);
   }
