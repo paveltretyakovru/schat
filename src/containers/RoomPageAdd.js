bcf38d5d0d47ad4aed696bb484d1e85d2ccbf65c
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-01-22 14:39:08 +0400

    Updated containers/RooPageAdd.js, created styles/roomAddPage.css

--- src/containers/RoomPageAdd.js
+++ src/containers/RoomPageAdd.js
@@ -1,9 +1,30 @@
 import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import FlatButton from 'material-ui/FlatButton';
+
+// STYLE SHEET
+import '../styles/roomAddPage.css';
 
 export default class RoomPageAdd extends Component {
   render() {
-    return(<div>
-      Room Page Add
+    return(<div className="row center-xs">
+      <div className="col-md-6 col-xs-6">
+        <TextField
+          hintText="Room title"
+          fullWidth={ true }
+          floatingLabelText="Room title"
+        />
+        <TextField
+          hintText="Room ID"
+          fullWidth={ true }
+          floatingLabelText="Room ID"
+        />
+        <div id="room-add-form-buttons">
+          <FlatButton label="Random" />          
+          <FlatButton label="Done" primary={true} />
+          <FlatButton label="Cancel" secondary={true} />
+        </div>
+      </div>
     </div>);
   }
 }
