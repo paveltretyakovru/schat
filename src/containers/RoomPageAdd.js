4f0877381658b0cbac8be7a5d55fc92a5dfdc4b2
Tretyakov Pavel <tretyakovpavel.ru@gmail.com>
2017-01-22 23:40:20 +0400

    Updated containers/RoomPageAdd.js

--- src/containers/RoomPageAdd.js
+++ src/containers/RoomPageAdd.js
@@ -12,31 +12,31 @@ import '../styles/roomAddPage.css';
 
 class RoomPageAdd extends Component {
   componentWillMount() {
-    this.props.appActions.updateHeaderTitle('Add new crypted chat');
+    this.props.appActions.updateHeaderTitle('Add new room');
     this.props.appActions.updateHeaderLeftIcon('close');
     this.props.appActions.updateHeaderRightIcon('save');
   }
 
   render() {
     return(<div className="row center-xs animated fadeInLeft">
-      <div className="col-md-6 col-xs-6">
+      <div className="col-md-4 col-xs-11">
         <TextField
           hintText="Room title"
           fullWidth={ true }
-          floatingLabelText="Room title"
+          floatingLabelText="Room title for your rooms list"
         />
         <TextField
           hintText="Room ID"
           fullWidth={ true }
-          floatingLabelText="Room ID"
+          floatingLabelText="Room ID for generating room link"
         />
         <TextField
           hintText="Room Key"
           fullWidth={ true }
-          floatingLabelText="Room Key"
+          floatingLabelText="Room Key to encrypt your messages"
         />
         <div id="room-add-form-buttons">
-          <FlatButton label="Random" />
+          <FlatButton primary={true} label="Generate random data" />
         </div>
       </div>
     </div>);
