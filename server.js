94c9351fe6d20c0f6f38e7181f7dbbbcbcf69a7f
Pavel <tretyakovpavel.ru@gmail.com>
2018-03-25 07:07:40 +0300

    Added first routes to server. Calced source of free space on right site side

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const app = new (require('express'))();
 const notifier = require('node-notifier');
 
+// Routers
+const appRouter = require('./src/backend/app.router')
+const userRouter = require('./src/backend/user/user.router')
+
 app.set('port', process.env.PORT || 3002);
 app.set('host', process.env.SERVER_HOST || 'localhost');
 app.set('frontHost', process.env.FRONT_HOST || 'localhost:8080');
 
-app.get('/', (req, res) => {
-  res.send('Hello schat server!');
-})
+app.use('/', appRouter)
+app.use('/user', userRouter)
 
 app.listen(app.get('port'), app.get('host'), (error) => {
   let mess = (error) ? error : `Server: ${app.get('host')}:${app.get('port')}/`;
